5eca2741e57b3c4e10d7b7523f2a687b7886998f
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-12 11:59:38 +0400

    list array

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Person : IComparable<Person>
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public int Age { get; set; }
+
+        public Person(int id,string name, string Title,int age ) 
+        { 
+            Id = id;
+            Name = name;
+            this.Title = Title;
+            Age = age;
+        }
+
+        public int CompareTo(Person? other)
+        {
+           if (other == null) return 1;
+           if (other.Age == this.Age) return 0;
+           if(other.Age>this.Age) return -1;
+            return 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Add(2);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Add(3);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Remove(0);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Insert(0,5);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            Console.WriteLine(list[0]);
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Person> personList = new List<Person>();
+
+            personList.Add(new Person(1101, "abc", "Male",23));
+            personList.Add(new Person(1102,"def","Femal",29));
+            personList.Add (new Person(1102, "ghi","male",45));
+            foreach (Person person in personList)
+            {
+                Console.WriteLine(person.Name);
+            }
+            personList.Sort();
+            //personList.Sort(Person.Id);
+            foreach (Person person in personList)
+            {
+                Console.WriteLine(person.ToString());
+            }
+            Console.WriteLine("ArrayLIst");
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("hello");
+            arrayList.Add(new Person(1102, "ghi", "male", 45));
+            foreach(var items in arrayList)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
